81a31808988f0bba91ccca4d2ce79c1592a2b299
Joao <joao@houzing.eu>
2022-08-09 22:42:34 +0100

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Joao Pedro Pereira on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
